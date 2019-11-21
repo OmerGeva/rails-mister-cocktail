1cec5ad836775696d894897bf68b59d302bf2d27
Omer Geva <ogeva97@gmail.com>
2019-11-21 11:52:11 +0200

    fixed root bug

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -15,7 +15,7 @@ class CocktailsController < ApplicationController
     @cocktail = Cocktail.new(cocktail_params)
     @cocktail.save
 
-    redirect_to cocktails_path
+    redirect_to '/'
   end
 
   private
